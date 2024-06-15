b43737c5b8f190e193ad68c8b47f23c1013e335c
wenzhuo4657 <14783149521@163.com>
2024-06-15 10:12:20 +0800

    backlink

---
+++ JDKAndCglib/src/main/java/cn/wenzhuo4657/jdk/stu.java
@@ -0,0 +1,13 @@
+package cn.wenzhuo4657.jdk;
+
+/**
+ * @className: stu
+ * @author: wenzhuo4657
+ * @date: 2024/6/14 17:19
+ * @Version: 1.0
+ * @description:
+ */
+public interface stu {
+    public  void goSchool();
+
+}
---
+++ JDKAndCglib/src/main/java/cn/wenzhuo4657/jdk/stuImpl.java
@@ -0,0 +1,19 @@
+package cn.wenzhuo4657.jdk;
+
+import cn.wenzhuo4657.jdk.stu;
+
+/**
+ * @className: stu
+ * @author: wenzhuo4657
+ * @date: 2024/6/14 16:20
+ * @Version: 1.0
+ * @description:
+ */
+public class stuImpl implements stu {
+    int code;
+    String name;
+    @Override
+    public  void goSchool() {
+        System.out.println("进去学校");
+    }
+}
